d2c006edb805bca1b35153efa7e25a422fce493b
ElLuchoMan <luchoman96@gmail.com>
2021-01-13 16:30:29 -0500

    Vistas terminadas, faltra cruzar información

--- src/app/components/citas/listar-citas/listar-citas.component.ts
+++ src/app/components/citas/listar-citas/listar-citas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CitasService } from 'src/app/services/citas.service';
 
 @Component({
   selector: 'app-listar-citas',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarCitasComponent implements OnInit {
 
-  constructor() { }
+  citas: any[]=[];
+  constructor(private _CitasService: CitasService,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.getTipoCitas();
+  }
+  getTipoCitas(){
+    this._CitasService.getCitas().subscribe(data=>{
+      data.forEach((element:any)=>{
+        this.citas.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+    })
+  }
+  eliminarTipoCita(id: string){
+    this._CitasService.eliminarCitas(id).then(()=>{
+      this.toastr.error('Tipo Cita eliminado con éxito','Tipo cita eliminado');
+    })
   }
 
 }
